6d73ac3596429a47317773c4b81bfb7281809e48
Masebav19 <mateo.vasquez1723@outlook.com>
2025-10-29 17:51:43 -0500

    Cambio de horario

--- celandarapp/src/utils/constants.js
+++ celandarapp/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const DAYSNAMES = ['Domingo','Lunes','Martes','Miercoles','Jueves', 'Viernes','Sábado']
-export const CLASSHOURS = ['06:00-07:00','07:00-09:00','09:00-11:00','11:00-13:00','14:00-16:00',
+export const CLASSHOURS = ['05:00-07:00','07:00-09:00','09:00-11:00','11:00-13:00','14:00-16:00',
     '16:00-18:00'
 ]
 export const MONTHNAMES = ['Enero', 'Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto',
